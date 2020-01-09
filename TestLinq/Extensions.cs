dc8df7c0978d4a457fcf6edb5263efb955e50548
Aleksey Skopintsev <skopka@skopka.ru>
2020-01-09 13:39:53 +0300

    Add project files.

---
+++ TestLinq/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestLinq
+{
+    internal static class Extensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            if (action == null)
+                throw new ArgumentNullException(nameof(action));
+
+            foreach (T item in source)
+                action(item);
+        }
+    }
+}
---
+++ TestLinq/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+namespace TestLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Program program = new Program();
+                program.LinqTest();
+            }
+            catch (Exception e)
+            {
+                WriteLine(e);
+            }
+            finally
+            {
+                WriteLine($"Press any key for exit");
+                ReadKey();
+            }
+        }
+
+        private void LinqTest()
+        {
+            var testList = new List<int> { 1, 2, 3 };
+            var testLinq = testList.Where(item => item > 2);
+
+            WriteEnumerable(testLinq, "Before adding");
+
+            testList.Add(4);
+
+            WriteEnumerable(testLinq, "After adding");
+        }
+
+        private void WriteEnumerable<T>(IEnumerable<T> enumerable, string text)
+        {
+            WriteLine(text);
+            enumerable.ForEach(item => Write($"{item} "));
+            WriteLine();
+            WriteLine();
+        }
+    }
+}
